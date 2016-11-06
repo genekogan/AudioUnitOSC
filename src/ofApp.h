ec4e63540b25d1c5496cb5082ff6bd47867322a5
Gene Kogan <kogan.gene@gmail.com>
2016-11-06 09:17:24 +0100

    refactor for multiple audio units and of_0.9.6

--- src/ofApp.h
+++ src/ofApp.h
@@ -6,96 +6,82 @@
 #include "AudioUnitController.h"
 
 
-class Selection {
-public:
-    void setAudioUnit(AudioUnitController *au) {
-        this->au = au;
-    }
-    void add(string name) {
-        parameters.push_back(name);
-    }
-    void remove(string name) {
-        parameters.erase(std::remove(parameters.begin(), parameters.end(), name), parameters.end());
-    }
-    void setValue(int idx, float value) {
-        if (idx < parameters.size()) {
-            float ival = ofLerp(au->getParameter(parameters[idx]).getMin(), au->getParameter(parameters[idx]).getMax(), value);
-            au->getParameter(parameters[idx]).set(ival);
-        }
-    }
-    void draw(int x, int y) {
-        ofPushMatrix();
-        ofPushStyle();
-        ofTranslate(x, y);
-        for (int i=0; i<parameters.size(); i++) {
-            ofSetColor(0);
-            ofDrawBitmapString(ofToString(i) + " :: " + parameters[i], 0, (i + 1) * 16);
-        }
-        ofPopStyle();
-        ofPopMatrix();
-    }
+struct ParamterAudioUnitPair {
+    ParamterAudioUnitPair(AudioUnitController *au, string name);
+    void setValue(float value);
     
     AudioUnitController *au;
-    vector<string> parameters;
+    string name;
+};
+
+
+class Selection {
+public:
+    void add(AudioUnitController *au, string name);
+    void remove(AudioUnitController *au, string name);
+    void removeAudioUnit(string audioUnitName);
+    void setValue(int idx, float value);
+    void draw(int x, int y);
+    
+    vector<ParamterAudioUnitPair> parameters;
 };
 
 
 class Section {
 public:
-    Section(AudioUnitController *au, string name, vector<AudioUnitParameterInfo> & parameters, ofxControlWidget *gui, Selection *selection) {
-        this->au = au;
-        this->gui = gui;
-        this->selection = selection;
-        vector<string> choices;
-        for (auto p : parameters) {
-            //if (p.flags == 3360686096) { // exponential
-            choices.push_back(p.name);
-        }
-        ofxControlMenu *menu = gui->addMenu(name, choices, this, &Section::eventSelectParameter, true);
-        menu->setCollapsed(true);
-    }
-    void eventSelectParameter(ofxControlMenuEventArgs & e) {
-        string name = e.toggle->getName();
-        if (e.value) {
-            selection->add(name);
-        }
-        else {
-            selection->remove(name);
-        }
-    }
+    Section(AudioUnitController *au, string audioUnitName, string sectionName, vector<AudioUnitParameterInfo> & parameters, ofxControlWidget *gui, Selection *selection);
+    void eventSelectParameter(ofxControlMenuEventArgs & e);
+    void remove();
 
     AudioUnitController *au;
     ofxControlWidget *gui;
     Selection *selection;
     ofxControlMenu *menu;
+    string audioUnitName, sectionName;
 };
 
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp
+{
+public:
+    void setup();
+    void update();
+    void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    void addAudioUnit(string name);
+    void removeAudioUnit(string name);
 		
-    AudioUnitController au;
+    void selectAudioUnit(ofxControlMenuEventArgs & evt);
+    void selectMidiDevice(ofxControlMenuEventArgs & evt){
+        currentMidiDevice = &units[evt.toggle->getName()];
+    }
+    
+    map<string, vector<string> > unitLookup;
+    map<string, AudioUnitController> units;
+    map<string, ofxControlMenu*> unitMenus;
+    
     ofxAudioUnitMixer mixer;
     ofxAudioUnitOutput output;
-    
-    ofxControlWidget gui;
+    int idxMixer;
 
+    ofxControlWidget gui;
+    ofxControlWidget synthGui;
+    ofxControlMenu *midiGui;
+
+    AudioUnitController *currentMidiDevice;
+    
     vector<Section*> sections;
     Selection selection;
     
